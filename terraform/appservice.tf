6acaf756bc1986836beac59c648fe364cd6ba490
ivan-saji <ivansajimeleth@gmail.com>
2026-08-06 13:03:03 +0530

    working on github actions

--- terraform/appservice.tf
+++ terraform/appservice.tf
@@ -15,8 +15,17 @@ resource "azurerm_linux_web_app" "appservice_lab" {
   location            = azurerm_resource_group.rg_appservice_lab.location
   resource_group_name = azurerm_resource_group.rg_appservice_lab.name
   service_plan_id     = azurerm_service_plan.asp_appservice_lab.id
+  https_only           = true
 
   site_config {
     always_on = false
   }
+}
+
+#Create deployment for Production
+
+resource "azurerm_app_source_control" "production_source_control" {
+  app_id                = azurerm_linux_web_app.appservice_lab.id
+  branch                = "main"
+  repo_url              = "https://github.com/ivan-saji/nodejs-docs-hello-world.git"
 }
