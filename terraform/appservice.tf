5cfb5c00c9f033d35d9375b4c98f182d1072ccb7
ivan-saji <ivansajimeleth@gmail.com>
2026-08-06 22:38:36 +0530

    Automated single slot deployment completed. Next to configure the staging slot and change tier

--- terraform/appservice.tf
+++ terraform/appservice.tf
@@ -19,13 +19,29 @@ resource "azurerm_linux_web_app" "appservice_lab" {
 
   site_config {
     always_on = false
+
+    application_stack {
+      node_version = "24-lts"
+    }
   }
 }
 
 #Create deployment for Production
 
-resource "azurerm_app_source_control" "production_source_control" {
+resource "azurerm_app_service_source_control" "production_source_control" {
   app_id                = azurerm_linux_web_app.appservice_lab.id
   branch                = "main"
   repo_url              = "https://github.com/ivan-saji/nodejs-docs-hello-world.git"
+
+
+  #Github action configuration is commented out because it is not required for this deployment. If you want to enable GitHub Actions for continuous deployment, you can uncomment the following block and configure it accordingly.
+  # github_action_configuration {
+  #   generate_workflow_file = true
+
+  #   code_configuration {
+  #     runtime_stack   = "node"
+  #     runtime_version = "24-lts"
+  #   }
+  # }
+
 }
